c1a013b6819663ebab39dd4be691493d9e92c2f2
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-09-23 23:57:23 +0200

    Random panel to start

--- scripts/utils.gd
+++ scripts/utils.gd
@@ -12,6 +12,70 @@ var global_piece_value = GLOBAL_PIECE_VALUES_ACCORDING_LEVEL["hard"] # Hard by d
 
 var win_color
 
+var empty_spaces_dictionary = [
+	{
+		key = '0',
+		value = PoolVector2Array([
+			Vector2(0,3),
+			Vector2(1,3),
+			Vector2(0,4),
+			Vector2(1,4),
+			Vector2(0,5),
+			Vector2(1,5),
+			Vector2(0,6),
+			Vector2(1,6),
+			Vector2(6,3),
+			Vector2(7,3),
+			Vector2(6,4),
+			Vector2(7,4),
+			Vector2(6,5),
+			Vector2(7,5),
+			Vector2(6,6),
+			Vector2(7,6)
+			])
+	},
+	{
+		key = '1',
+		value = PoolVector2Array([
+			Vector2(0,0),
+			Vector2(7,0),
+			Vector2(0,9),
+			Vector2(7,9),
+			Vector2(3,4),
+			Vector2(4,4),
+			Vector2(3,5),
+			Vector2(4,5)
+			])
+	},
+	{
+		key = '2',
+		value = PoolVector2Array([
+			Vector2(0,9),
+			Vector2(1,9),
+			Vector2(0,8),
+			Vector2(1,8),
+			Vector2(6,9),
+			Vector2(7,9),
+			Vector2(6,8),
+			Vector2(7,8),
+			Vector2(1,0),
+			Vector2(2,0),
+			Vector2(3,0),
+			Vector2(4,0),
+			Vector2(5,0),
+			Vector2(6,0),
+			Vector2(3,1),
+			Vector2(4,1)
+			])
+	}
+]
+
+func get_random_empty_spaces(rand):
+	return empty_spaces_dictionary[rand].value
+
+func get_empty_spaces_dictionary_size():
+	return empty_spaces_dictionary.size()
+
 func initialize_preset_spaces():
 	return PoolVector3Array([
 			Vector3(0, 0, 0),
