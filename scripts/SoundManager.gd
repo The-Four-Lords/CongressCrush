38c74fa3a39bc9ba873b66a8efb6ac8d0439e37b
Munoz Dominguez <jomu@gft.com>
2019-10-04 12:13:49 +0200

    fix sounds and buttons problems and optimize soundmanager logic

--- scripts/SoundManager.gd
+++ scripts/SoundManager.gd
@@ -11,10 +11,11 @@ var COMBO_LEVEL = 5
 var WIN_LEVEL = -10
 var MUTE = -100
 
+# Main theme
 var possible_music = [
 preload("res://art/Sound/Music/theme-1.ogg")
 ]
-
+# Effects for pieces and buttons
 var possible_sounds = [
 preload("res://art/Sound/Effects/Match1.wav"),
 preload("res://art/Sound/Effects/Match2.wav"),
@@ -24,40 +25,67 @@ preload("res://art/Sound/Effects/Match5.wav"),
 preload("res://art/Sound/Effects/Match6.wav"),
 preload("res://art/Sound/Effects/Match7.wav")
 ]
-
+# Game Win sounds
 var win_sounds = {
 "blue" : preload("res://art/Sound/Music/WIN/blue.ogg"),
 "red" : preload("res://art/Sound/Music/WIN/red.ogg"),
-"green" : preload("res://art/Sound/Music/WIN/green.ogg"),
+"green" : preload("res://art/Sound/Music/WIN/green_.ogg"),
 "orange" : preload("res://art/Sound/Music/WIN/orange.ogg"),
 "purple" : preload("res://art/Sound/Music/WIN/purple.ogg"),
 "yellow" : preload("res://art/Sound/Music/WIN/yellow.ogg"),
 "lose" : preload("res://art/Sound/Music/WIN/lose.ogg"),
 "draw" : preload("res://art/Sound/Music/WIN/draw.ogg")
 }
-
+# Combo sounds for blue pieces
+var combo_sounds_blue = [
+preload("res://art/Sound/Effects/Cortes/blue1.ogg"),
+preload("res://art/Sound/Effects/Cortes/blue2.ogg"),
+preload("res://art/Sound/Effects/Cortes/blue3.ogg")
+]
+# Combo sounds for purple pieces
+var combo_sounds_purple = [
+preload("res://art/Sound/Effects/Cortes/purple1.ogg"),
+preload("res://art/Sound/Effects/Cortes/purple2.ogg"),
+preload("res://art/Sound/Effects/Cortes/purple3.ogg")
+]
+# Combo sounds for green pieces
+var combo_sounds_green = [
+preload("res://art/Sound/Effects/Cortes/green1.ogg"),
+preload("res://art/Sound/Effects/Cortes/green2.ogg")
+]
+# Combo sounds for orange pieces
+var combo_sounds_orange = [
+preload("res://art/Sound/Effects/Cortes/orange1.ogg"),
+preload("res://art/Sound/Effects/Cortes/orange2.ogg"),
+preload("res://art/Sound/Effects/Cortes/orange3.ogg")
+]
+# Combo sounds for red pieces
+var combo_sounds_red = [
+preload("res://art/Sound/Effects/Cortes/red1.ogg"),
+preload("res://art/Sound/Effects/Cortes/red2.ogg")
+]
+# Combo sounds for yellow pieces
+var combo_sounds_yellow = [
+preload("res://art/Sound/Effects/Cortes/yellow1.ogg"),
+preload("res://art/Sound/Effects/Cortes/yellow2.ogg"),
+preload("res://art/Sound/Effects/Cortes/yellow3.ogg"),
+preload("res://art/Sound/Effects/Cortes/yellow4.ogg")
+]
+# Combo sounds for color pieces
+var combo_sounds_color = [
+preload("res://art/Sound/Effects/Cortes/color.ogg")
+]
+# All combo sounds
 var combo_sounds = {
-"blue1" : preload("res://art/Sound/Effects/Cortes/blue1.ogg"),
-"blue2" : preload("res://art/Sound/Effects/Cortes/blue2.ogg"),
-"blue3" : preload("res://art/Sound/Effects/Cortes/blue3.ogg"),
-"purple1" : preload("res://art/Sound/Effects/Cortes/purple1.ogg"),
-"purple2" : preload("res://art/Sound/Effects/Cortes/purple2.ogg"),
-"purple3" : preload("res://art/Sound/Effects/Cortes/purple2.ogg"),
-"green1" : preload("res://art/Sound/Effects/Cortes/green1.ogg"),
-"green2" : preload("res://art/Sound/Effects/Cortes/green2.ogg"),
-"green3" : preload("res://art/Sound/Effects/Cortes/green2.ogg"),
-"orange1" : preload("res://art/Sound/Effects/Cortes/orange1.ogg"),
-"orange2" : preload("res://art/Sound/Effects/Cortes/orange2.ogg"),
-"orange3" : preload("res://art/Sound/Effects/Cortes/orange3.ogg"),
-"red1" : preload("res://art/Sound/Effects/Cortes/red1.ogg"),
-"red2" : preload("res://art/Sound/Effects/Cortes/red2.ogg"),
-"red3" : preload("res://art/Sound/Effects/Cortes/red3.ogg"),
-"yellow1" : preload("res://art/Sound/Effects/Cortes/yellow1.ogg"),
-"yellow2" : preload("res://art/Sound/Effects/Cortes/yellow2.ogg"),
-"yellow3" : preload("res://art/Sound/Effects/Cortes/yellow3.ogg"),
-"color" : preload("res://art/Sound/Effects/Cortes/color.ogg")
+"blue" : combo_sounds_blue,
+"purple" : combo_sounds_purple,
+"green" : combo_sounds_green,
+"orange" : combo_sounds_orange,
+"red" : combo_sounds_red,
+"yellow" : combo_sounds_yellow,
+"color" : combo_sounds_color
 }
-
+# @Deprecated
 var possible_combo_sound = [
 preload("res://art/Sound/Effects/delisioso.ogg")
 ]
@@ -78,22 +106,20 @@ func play_random_sound():
 		sound_player.stream = possible_sounds[temp]
 		sound_player.play()
 
-#Deprecated - now is play_combo_sound(color)
+# @Deprecated - now is play_combo_sound(color)
 func play_random_combo_sound():
 	if possible_combo_sound.size() > 0:
 		var temp = floor(rand_range(0, possible_combo_sound.size()))
 		combo_sound_player.stream = possible_combo_sound[temp]
 		combo_sound_player.play()
-		
+
 func play_combo_sound(color):
-	#print("DEBUG - color:",color)
-	if (color == "color"):
-		combo_sound_player.stream = combo_sounds[color]
-		combo_sound_player.play()
-		return
-	var number = floor(rand_range(1, 4))
-	var sound = color + String(number)
-	combo_sound_player.stream = combo_sounds[sound]
+	var sounds_list = combo_sounds[color]
+	var list_size = sounds_list.size()
+	var sound_index = floor (rand_range(0, list_size))
+	var sound = sounds_list[sound_index]
+	print("DEBUG - color:", color, " list_size:", list_size, " sound_index:", sound_index, " sound:", sound)
+	combo_sound_player.stream = sound
 	combo_sound_player.play()
 
 func stop_combo_sound():
